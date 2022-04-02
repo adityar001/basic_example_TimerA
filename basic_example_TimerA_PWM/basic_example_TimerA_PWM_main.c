4013a0350f9b272ddecf535a00f38bf69d7eaba2
Zayeem Zaman <zayeem@vt.edu>
2022-04-01 22:02:20 -0400

    Fixed LED bug

--- basic_example_TimerA_PWM/basic_example_TimerA_PWM_main.c
+++ basic_example_TimerA_PWM/basic_example_TimerA_PWM_main.c
@@ -1,9 +1,17 @@
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 #include "ButtonLED_HAL.h"
 
+// Comment out the following macro to switch between
+// Timer32 and Timer_A.
+#define TIMER32 1
 
-//#define TIMER32 1
+// Modify the BLOCKING macro below to activate/deactivate
+// the blocking code. Notice the difference in performance.
+#define BLOCKING    1
+#define BLOCK_COUNT 1000000
 
+// This function initializes all the peripherals except graphics
+void initialize();
 
 #ifdef TIMER32
 
@@ -22,16 +30,14 @@
 #define ON_CYCLES     (DUTY_CYCLE_FRACTION * PWM_PERIOD_CYLCLES)
 #define OFF_CYCLES    ((1-DUTY_CYCLE_FRACTION) * PWM_PERIOD_CYLCLES)
 
-typedef enum {ON, OFF} pwm_state_t;
-
-#define BLOCKING    0
-#define BLOCK_COUNT 100000
-
-// This function initializes all the peripherals except graphics
-void initialize();
+typedef enum
+{
+    ON, OFF
+} pwm_state_t;
 
 // We are using TIMER32_0_BASE as the On timer
-void startOnTimer(){
+void startOnTimer()
+{
     Timer32_setCount(TIMER32_0_BASE, ON_CYCLES);
     Timer32_startTimer(TIMER32_0_BASE, true);
 }
@@ -41,8 +47,8 @@ bool OnTimerExpired()
     return (Timer32_getValue(TIMER32_0_BASE) == 0);
 }
 
-
-void startOffTimer(){
+void startOffTimer()
+{
     Timer32_setCount(TIMER32_1_BASE, OFF_CYCLES);
     Timer32_startTimer(TIMER32_1_BASE, true);
 }
@@ -59,7 +65,7 @@ int main(void)
 
     startOffTimer();
     pwm_state_t pwmState = OFF;
-    int i;
+    volatile int i; // Volatile is used here to prevent compiler optimizations from getting rid of this variable.
 
     while (1)
     {
@@ -71,7 +77,8 @@ int main(void)
             // The BLOCK_COUNT and BLOCKING macros control this blocking loop
             // This blocking code makes the "ON" state longer than it should be and disturbs the correct duty cycle
             turnOn_LaunchpadLED1();
-            for (i=0; i<(BLOCK_COUNT * BLOCKING); i++);
+            for (i = 0; i < (BLOCK_COUNT * BLOCKING); i++)
+                ;
             turnOff_LaunchpadLED1();
 
             pwmState = OFF;
@@ -90,43 +97,7 @@ int main(void)
     }
 }
 
-
-void initialize()
-{
-    // stop the watchdog timer
-    WDT_A_hold(WDT_A_BASE);
-
-
-
-    Timer32_initModule(TIMER32_0_BASE, // There are two timers, we are using the one with the index 0
-                       TIMER32_PRESCALER_1, // The prescaler value is 1; The clock is not divided before feeding the counter
-                       TIMER32_32BIT, // The counter is used in 32-bit mode; the alternative is 16-bit mode
-                       TIMER32_PERIODIC_MODE); //This options is irrelevant for a one-shot timer
-
-    Timer32_initModule(TIMER32_1_BASE, // There are two timers, we are using the one with the index 1
-                       TIMER32_PRESCALER_1, // The prescaler value is 1; The clock is not divided before feeding the counter
-                       TIMER32_32BIT, // The counter is used in 32-bit mode; the alternative is 16-bit mode
-                       TIMER32_PERIODIC_MODE); //This options is irrelevant for a one-shot timer
-
-    initialize_BoosterpackLED_blue();
-    turnOff_BoosterpackLED_blue();
-
-    initialize_LaunchpadLED2_green();
-    turnOff_LaunchpadLED2_green();
-
-    initialize_LaunchpadLED1();
-    turnOff_LaunchpadLED1();
-
-
-}
-
-
-
 #else
-
-// This function initializes all the peripherals except graphics
-void initialize();
-
 
 //The number of counter cycles during 1s based on a source clock of 3MHz and the divider of 64
 #define ONE_SEC_COUNT  46875
@@ -159,70 +130,95 @@ void initialize();
 #define BLU_TIMER    TIMER_A2_BASE
 #define BLU_CHANNEL  TIMER_A_CAPTURECOMPARE_REGISTER_1
 
-#define BLOCKING    0
-#define BLOCK_COUNT 100000
-
 void initPWM()
 {
     // For the Blue LED, we first configure it as a GPIO and turn it off
     // Then, we configure it to work as output channel of a timer
     // The blue LED on booster is wried to Port 5, Pin 6, we cannot change this as the programmer
     // BLUE LED
-    GPIO_setAsOutputPin    (GPIO_PORT_P5,    GPIO_PIN6);
-    GPIO_setOutputLowOnPin (GPIO_PORT_P5,    GPIO_PIN6);
+    GPIO_setAsOutputPin(GPIO_PORT_P5, GPIO_PIN6);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P5, GPIO_PIN6);
     GPIO_setAsPeripheralModuleFunctionOutputPin(
-            GPIO_PORT_P5,
-            GPIO_PIN6,
-            GPIO_PRIMARY_MODULE_FUNCTION);
+    GPIO_PORT_P5,
+                                                GPIO_PIN6,
+                                                GPIO_PRIMARY_MODULE_FUNCTION);
 }
 
+int main(void)
+{
 
-int main(void) {
+    // Stop WDT
+    WDT_A_holdTimer();
 
-  // Stop WDT
-  WDT_A_holdTimer();
+    initialize();
 
+    Timer_A_PWMConfig pwmConfig_blu;
 
-  Timer_A_PWMConfig pwmConfig_blu;
+    // The source clock is the system clock (3MHz)
+    pwmConfig_blu.clockSource = TIMER_A_CLOCKSOURCE_SMCLK;
 
-  // The source clock is the system clock (3MHz)
-  pwmConfig_blu.clockSource = TIMER_A_CLOCKSOURCE_SMCLK;
+    // The clock divider is 64
+    pwmConfig_blu.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_64;
 
-  // The clock divider is 64
-  pwmConfig_blu.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_64;
+    // The # of counter cycles in one PWM round (period of PWM in terms of counter cycle)
+    pwmConfig_blu.timerPeriod = PWM_PERIOD_CYLCLES;
 
-  // The # of counter cycles in one PWM round (period of PWM in terms of counter cycle)
-  pwmConfig_blu.timerPeriod = PWM_PERIOD_CYLCLES;
+    // The OC mode. We choose this such that we can achieve the waveform we are interested in.
+    pwmConfig_blu.compareOutputMode = TIMER_A_OUTPUTMODE_SET_RESET;
 
-  // The OC mode. We choose this such that we can achieve the waveform we are interested in.
-  pwmConfig_blu.compareOutputMode = TIMER_A_OUTPUTMODE_SET_RESET;
+    // The OC value. This is the value that when the counter hits it, something happens to the waveform
+    // Since we chose SET_RESET, at this count the GPIO is set and the LED is turned on
+    // This means the LED was off until hitting this count and therefore, it is the off-cycle count for us
+    pwmConfig_blu.dutyCycle = BLU_COMPARE_CYCLES;
 
-  // The OC value. This is the value that when the counter hits it, something happens to the waveform
-  // Since we chose SET_RESET, at this count the GPIO is set and the LED is turned on
-  // This means the LED was off until hitting this count and therefore, it is the off-cycle count for us
-  pwmConfig_blu.dutyCycle = BLU_COMPARE_CYCLES;
+    // The output register used for OC operation.
+    // The programmer has to choose this such that it feeds to the right pin connected to the device needing the PWM.
+    pwmConfig_blu.compareRegister = BLU_CHANNEL;
 
-  // The output register used for OC operation.
-  // The programmer has to choose this such that it feeds to the right pin connected to the device needing the PWM.
-  pwmConfig_blu.compareRegister = BLU_CHANNEL;
+    initPWM();
 
-  initPWM();
+    // The below line starts generating the pulse
+    Timer_A_generatePWM(BLU_TIMER, &pwmConfig_blu);
 
-  // The below line starts generating the pulse
-  Timer_A_generatePWM(BLU_TIMER, &pwmConfig_blu);
+    volatile int i; // Volatile is used here to prevent compiler optimizations from getting rid of this variable.
 
-  int i;
+    while (1)
+    {
+        // The blocking code.
+        turnOn_LaunchpadLED1();
+        for (i = 0; i < (BLOCK_COUNT * BLOCKING); i++)
+            ;
+        turnOff_LaunchpadLED1();
 
-  while (1)
-  {
-      // The blocking code.
-      turnOn_LaunchpadLED1();
-      for (i=0; i<(BLOCK_COUNT * BLOCKING); i++);
-      turnOff_LaunchpadLED1();
-
-      // Nothing needs to be done here for handling the pulse as the Timer_A automatically takes care of it and the processor is hands off.
-  }
+        // Nothing needs to be done here for handling the pulse as the Timer_A automatically takes care of it and the processor is hands off.
+    }
 
 }
 
 #endif
+
+void initialize()
+{
+    // stop the watchdog timer
+    WDT_A_hold(WDT_A_BASE);
+
+    Timer32_initModule(TIMER32_0_BASE, // There are two timers, we are using the one with the index 0
+            TIMER32_PRESCALER_1, // The prescaler value is 1; The clock is not divided before feeding the counter
+            TIMER32_32BIT, // The counter is used in 32-bit mode; the alternative is 16-bit mode
+            TIMER32_PERIODIC_MODE); //This options is irrelevant for a one-shot timer
+
+    Timer32_initModule(TIMER32_1_BASE, // There are two timers, we are using the one with the index 1
+            TIMER32_PRESCALER_1, // The prescaler value is 1; The clock is not divided before feeding the counter
+            TIMER32_32BIT, // The counter is used in 32-bit mode; the alternative is 16-bit mode
+            TIMER32_PERIODIC_MODE); //This options is irrelevant for a one-shot timer
+
+    initialize_BoosterpackLED_blue();
+    turnOff_BoosterpackLED_blue();
+
+    initialize_LaunchpadLED2_green();
+    turnOff_LaunchpadLED2_green();
+
+    initialize_LaunchpadLED1();
+    turnOff_LaunchpadLED1();
+
+}
